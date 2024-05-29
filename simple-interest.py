965424c348b53ee55135d4c8c5d2df27d5024292
Mia-Bellla <156600352+Mia-Bellla@users.noreply.github.com>
2024-05-29 11:50:39 +0530

    Add files via upload

---
+++ simple-interest.py
@@ -0,0 +1,16 @@
+
+def simple_interest(p, t, r):
+    print('The principal is', p)
+    print('The time period is', t)
+    print('The rate of interest is', r)
+    
+    si = (p * t * r) / 100
+    
+    print('The Simple Interest is', si)
+    return si
+
+
+p, t, r = map(int, input("Enter three numbers: ").split())
+
+
+simple_interest(p, t, r)
