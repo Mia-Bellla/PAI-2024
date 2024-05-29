713291627e97bf429a26e3babde912ebadbc907c
Mia-Bellla <156600352+Mia-Bellla@users.noreply.github.com>
2024-05-29 11:32:38 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+ 
+# Adding two nos
+sum = num1 + num2
+ 
+# printing values
+print("Sum of", num1, "and", num2 , "is", sum)
